c279b4822e0f8a872bac344e5053179cf7e9cca8
Dipesh <bdipesh304@gmail.com>
2023-07-10 17:21:17 +0545

    Api endpoint output

---
+++ Scrap/api.py
@@ -0,0 +1,16 @@
+{
+        english_day: 1,
+        nepali_day: 12,
+        day:Monday,
+        Tithi:"Dashmai",
+        events: {
+            {Name:None,ColorCode:None},{Name:None,ColorCode:None},{Name:None,ColorCode:None}
+},
+        {
+        english_day: 1,
+        nepali_day: 12,
+        day:Monday,
+        Tithi:"Dashmai",
+        events: {
+            {Name:None,ColorCode:None},{Name:None,ColorCode:None},{Name:None,ColorCode:None}
+}
