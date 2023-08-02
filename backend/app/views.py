20f7e74be913e3279cd0d4c9bc37c43b872b968b
Dipesh <bdipesh304@gmail.com>
2023-08-02 23:30:09 +0545

    created api view for query

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.generics import ListAPIView
 
-# Create your views here.
+month_dict = {
+    1: "January",
+    2: "February",
+    3: "March",
+    4: "April",
+    5: "May",
+    6: "June",
+    7: "July",
+    8: "August",
+    9: "September",
+    10: "October",
+    11: "November",
+    12: "December",
+}
+
+
+class caleander(ListAPIView):
+    pass
