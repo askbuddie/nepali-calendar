56a6e649afac66c6783eef71ef1f0588e65fb11d
Dipesh <bdipesh304@gmail.com>
2023-08-07 22:14:44 +0545

    Changed the tech stack from django to flask

--- Scrap/store.py
+++ Scrap/store.py
@@ -3,14 +3,14 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 
-engine = create_engine("sqlite://", echo=True)
+engine = create_engine("sqlite:///storage.db", echo=True)
 Base = declarative_base()
 
 
 class Date(Base):
     __tablename__ = "date"
 
-    date = Column(Date, primary_key=True)
+    date = Column(String, primary_key=True)
     NepDate = Column(String)
     EngDate = Column(String)
     Day = Column(String)
@@ -32,13 +32,64 @@ Base.metadata.create_all(engine)
 from sqlalchemy.orm import Session
 
 
-def add(date,Events, Color_code, day, tithi, EngDate, NepDate):
+def search_data_by_year_and_month(year, month):
     with Session(engine) as session:
-        
-        date1 = Date(date=date, Day=day, tithi=tithi, EngDate=EngDate, NepDate=NepDate)
-        session.add(date1)
-        for item1, item2 in zip(Events, Color_code):
-            event = Event(date=date, title=item1, color=item2)
-            session.add(event)
+        # Form the search string for the year and month (e.g., "2078-10%")
+        search_string = f"{year}-{month:02}%"
 
-        session.commit()
+        # Query the Date objects with the search string
+        queried_dates = session.query(Date).filter(Date.date.like(search_string)).all()
+        print(queried_dates)
+
+        if queried_dates:
+            results = []
+
+            for queried_date in queried_dates:
+                events = queried_date.events
+                result = {
+                    "date": queried_date.date,
+                    "Nepali Date": queried_date.NepDate,
+                    "English Date": queried_date.EngDate,
+                    "Day": queried_date.Day,
+                    "Tithi": queried_date.tithi,
+                    "events": [],
+                }
+
+                for event in events:
+                    result["events"].append(
+                        {"Event Title": event.title, "Event Color": event.color}
+                    )
+
+                results.append(result)
+
+            return results
+
+    return None
+
+
+# Example usage
+year_to_search = "2080"  # Replace with the year you want to search
+month_to_search = "11"  # Replace with the month you want to search
+
+queried_data = search_data_by_year_and_month(year_to_search, month_to_search)
+i = 1
+if queried_data:
+    for data in queried_data:
+        print("Date:", data["date"])
+        print("Nepali Date:", data["Nepali Date"])
+        print("English Date:", data["English Date"])
+        print("Day:", data["Day"])
+        print("Tithi:", data["Tithi"])
+        i += 1
+
+        events = data["events"]
+        if events:
+            print("Events:")
+            for event in events:
+                print("Event Title:", event["Event Title"])
+                print("Event Color:", event["Event Color"])
+        else:
+            print("No events found for the date.")
+else:
+    print("No data found for the specified year and month.")
+print(i)
---
+++ backend/main.py
@@ -0,0 +1,115 @@
+from sqlalchemy import Column, Date, String, ForeignKey, Integer
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+
+engine = create_engine("sqlite:///storage.db", echo=True)
+Base = declarative_base()
+
+
+class Date(Base):
+    __tablename__ = "date"
+
+    date = Column(String, primary_key=True)
+    NepDate = Column(String)
+    EngDate = Column(String)
+    Day = Column(String)
+    tithi = Column(String)
+
+
+class Event(Base):
+    __tablename__ = "event"
+
+    event_id = Column(Integer, autoincrement=True, primary_key=True)
+    date = Column(ForeignKey("date.date"))
+    title = Column(String)
+    color = Column(String)
+
+    date_rel = relationship("Date", backref="events")
+
+
+Base.metadata.create_all(engine)
+from sqlalchemy.orm import Session
+
+
+def search_data_by_year_and_month(year, month):
+    with Session(engine) as session:
+        # Form the search string for the year and month (e.g., "2078-10%")
+        search_string = f"{year}-{month}%"
+
+        # Query the Date objects with the search string
+        queried_dates = session.query(Date).filter(Date.date.like(search_string)).all()
+        print(queried_dates)
+
+        if queried_dates:
+            results = []
+
+            for queried_date in queried_dates:
+                events = queried_date.events
+                result = {
+                    "date": queried_date.date,
+                    "Nepali Date": queried_date.NepDate,
+                    "English Date": queried_date.EngDate,
+                    "Day": queried_date.Day,
+                    "Tithi": queried_date.tithi,
+                    "events": [],
+                }
+
+                for event in events:
+                    result["events"].append(
+                        {"Event Title": event.title, "Event Color": event.color}
+                    )
+
+                results.append(result)
+
+            return results
+
+    return None
+
+
+
+nepali_month_dict = {
+    "Baishakh": 1,
+    "Jestha": 2,
+    "Ashadh": 3,
+    "Shrawan": 4,
+    "Bhadra": 5,
+    "Ashwin": 6,
+    "Kartik": 7,
+    "Mangsir": 8,
+    "Poush": 9,
+    "Magh": 10,
+    "Falgun": 11,
+    "Chaitra": 12
+}
+
+
+
+from flask import Flask,request
+from bleach import clean
+app = Flask(__name__)
+
+
+years=[2080]
+
+@app.route('/calender',methods=["GET"])
+def main():
+    # Example usage
+    args = request.args
+    year_to_search = clean(args.get("year"))  # Replace with the year you want to search
+    month_index=nepali_month_dict.get(clean(args.get("month") ))
+    if int(year_to_search) in years:
+        if month_index is not None:            
+            queried_data = search_data_by_year_and_month(year_to_search, month_index)
+            return queried_data
+            
+        else:
+            return"You have eneterd wrong month name!"
+        
+        
+    else:
+        return "Data does not exist",400
+
+  
+if __name__ == '__main__':
+    app.run()
